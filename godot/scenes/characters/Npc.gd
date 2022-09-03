119f62410704a5d18d32e9e5b33d17d743583075
floppyhammer <tannhauser_chen@outlook.com>
2022-09-03 15:58:53 +0800

    Add some level assets

--- godot/scenes/characters/Npc.gd
+++ godot/scenes/characters/Npc.gd
@@ -4,6 +4,9 @@ extends KinematicBody2D
 This is the base of all NPCs.
 """
 
+signal player_come_close
+signal player_goes_away
+
 
 func _ready():
 	$Bubble.hide()
@@ -22,9 +25,11 @@ func interact():
 
 func _on_InteractionArea_body_entered(body):
 	if body.is_in_group("player"):
+		emit_signal("player_come_close")
 		$Bubble.show()
 
 
 func _on_InteractionArea_body_exited(body):
 	if body.is_in_group("player"):
+		emit_signal("player_goes_away")
 		$Bubble.hide()
