b68f634bd73a225966e3263a97adeca395583fb1
floppyhammer <tannhauser_chen@outlook.com>
2022-10-26 19:43:55 +0800

    Tidy

---
+++ ci_test.ps1
@@ -0,0 +1,3 @@
+# Run all unit tests using Gut.
+godot.exe -d -s --path "../godot" addons/gut/gut_cmdln.gd -gdir=res://unit_tests -ginclude_subdirs -gexit
+pause
--- ci_test.sh
+++
@@ -1,1 +0,0 @@
-godot.windows.opt.tools.64.exe -d -s --path "./godot" addons/gut/gut_cmdln.gd -gdir=res://test -ginclude_subdirs -gexit
