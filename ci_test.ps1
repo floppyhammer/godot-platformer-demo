4c3cba84fcffb525e7cd7fb6d03b5e7d33dc9a10
floppyhammer <tannhauser_chen@outlook.com>
2023-02-23 16:42:26 +0800

    Delete unused assets

--- ci_test.ps1
+++
@@ -1,3 +0,0 @@
-# Run all unit tests using Gut.
-godot.exe -d -s --path "../godot" addons/gut/gut_cmdln.gd -gdir=res://unit_tests -ginclude_subdirs -gexit
-pause
--- godot/unit_tests/test_example.gd
+++
@@ -1,5 +0,0 @@
-extends "res://addons/gut/test.gd"
-
-
-func test_assert_true_with_true():
-	assert_true(true, "Should pass, true is true")
